004b3b0e7c8a757c06ebab4726ccab3958cc8e6d
Ecode2 <ecode5814@gmail.com>
2024-12-24 22:48:03 +0100

    removed security issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import Router from './Router'
 import AuthProvider from './auth/AuthContext'
 
 function App() {
-    console.log(import.meta.env.VITE_APP_API_URL)
     
     return (
       <AuthProvider>
